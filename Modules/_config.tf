bf4441e48b3000b4385b2f118d1d51d8cdd68a19
Razeen <Razeen.Abdal-Rahman@outlook.com>
2024-12-28 22:59:44 +0000

    add provider

--- Modules/_config.tf
+++ Modules/_config.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
+        source  = "hashicorp/aws"
         version = "~> 5.82.0"
     }
   }
+}
+
+provider "aws" {
+  region = "eu-west-1"
 }
